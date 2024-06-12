8202708140abad101f80122035969bd3a0d4ad8d
Kevin Schweikert <kevin@kevinschweikert.de>
2024-06-12 22:32:18 +0200

    relax elixir requirements

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule LivePremier.MixProject do
     [
       app: :live_premier,
       version: "0.1.0",
-      elixir: "~> 1.16",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -21,10 +21,11 @@ defmodule LivePremier.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:req, "~> 0.5"},
-      {:plug, "~> 1.0", only: [:test]},
+      {:credo, "~> 1.7"},
       {:ecto, "~> 3.11"},
-      {:ex_doc, "~> 0.34.0", only: [:dev]}
+      {:ex_doc, "~> 0.34.0", only: [:dev]},
+      {:plug, "~> 1.0", only: [:test]},
+      {:req, "~> 0.5"}
     ]
   end
 end
