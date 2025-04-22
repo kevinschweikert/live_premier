89fa8ce360956d5e11355ead25678d31fc6fda44
Kevin Schweikert <kevin@kevinschweikert.de>
2025-04-22 23:56:19 +0200

    ci: bump minimum elixir version to 1.14

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule LivePremier.MixProject do
     [
       app: :live_premier,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       name: "LivePremier",
